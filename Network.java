e91271f8336db53e25e4ebec44b058c6e7c2a5b8
OmerMesika1 <123023823+OmerMesika1@users.noreply.github.com>
2025-01-05 16:21:24 +0200

    changes

--- Network.java
+++ Network.java
@@ -131,9 +131,9 @@ public class Network {
        String ans = "Network:";
        for(int i=0;i<userCount;i++) {
         ans+= "\n";
-        ans += users[i].getName();
+        ans += users[i].getName()+" -> ";
         for(int j=0;j<users[i].getfCount();j++) {
-            ans += " -> " + users[i].getfFollows()[j] + " ";
+            ans +=  + users[i].getfFollows()[j] + " ";
         }
        }
        return ans;
